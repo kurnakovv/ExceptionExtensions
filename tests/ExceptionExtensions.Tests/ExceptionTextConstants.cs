7e97aad0a92036d1f2106f4de2ba5fa5941a6cb4
KurnakovMaksim <maksk210@gmail.com>
2022-05-10 17:31:39 +0900

    #7 Delete StackTrace from ex message for tests

--- tests/ExceptionExtensions.Tests/ExceptionTextConstants.cs
+++ tests/ExceptionExtensions.Tests/ExceptionTextConstants.cs
@@ -4,8 +4,6 @@
     {
         public const string INDEX_WAS_OUTSIDE_THE_BOUNDS_OF_THE_ARRAY_MESSAGE = @"==========================================
 Index was outside the bounds of the array.
-==========================================
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-   at ExceptionExtensions.Tests.ExceptionExtensionsTests.GetFullInfo_CanGetFullExceptionInfo_FullException() in";
+==========================================";
     }
 }
