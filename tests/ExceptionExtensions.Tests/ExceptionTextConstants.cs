a8691acd906f83698beb924803c164dbbe9d05ac
kurnakovv <maksk210@gmail.com>
2023-09-06 22:07:57 +0900

    #14 Try to fix GitHub tests

--- tests/ExceptionExtensions.Tests/ExceptionTextConstants.cs
+++ tests/ExceptionExtensions.Tests/ExceptionTextConstants.cs
@@ -2,7 +2,10 @@
 {
     public class ExceptionTextConstants
     {
-        public const string SOURCE_MESSAGE = "Source: ExceptionExtensions.Tests\r\n#################################";
-        public const string INDEX_WAS_OUTSIDE_THE_BOUNDS_OF_THE_ARRAY_MESSAGE = SOURCE_MESSAGE + "\r\nIndex was outside the bounds of the array.\r\n==========================================";
+        public const string SOURCE_MESSAGE = @"Source: ExceptionExtensions.Tests
+#################################";
+        public const string INDEX_WAS_OUTSIDE_THE_BOUNDS_OF_THE_ARRAY_MESSAGE = SOURCE_MESSAGE + @"
+Index was outside the bounds of the array.
+==========================================";
     }
 }
